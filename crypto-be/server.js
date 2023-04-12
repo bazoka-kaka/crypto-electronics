dc07d5b7a17e3e5edbc4d79272f01bc4a82ba252
bazoka-kaka <tsukikagemaikaka1@gmail.com>
2023-04-12 14:47:58 +0700

    add backend api

---
+++ crypto-be/server.js
@@ -0,0 +1,36 @@
+const { logger, errLogger } = require("./middlewares/logEvents");
+const express = require("express");
+const app = express();
+const path = require("path");
+const PORT = process.env.PORT || 3500;
+
+// middlewares
+app.use(logger);
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+// routes
+app.use("/", require("./routes/root"));
+
+app.all("*", (req, res) => {
+  res.status(404);
+
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ message: "404 - not found." });
+  } else {
+    res.type("txt").send("404 - not found.");
+  }
+});
+
+// errLogger middleware
+app.use(errLogger);
+
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+});
