f96e3456a9a02f0b87a6a4b5b7339bc58fcef0c6
bazoka-kaka <tsukikagemaikaka1@gmail.com>
2023-04-12 16:32:25 +0700

    add verifyRoles middleware in api

---
+++ crypto-be/middlewares/verifyRoles.js
@@ -0,0 +1,14 @@
+const verifyRoles = (...allowedRoles) => {
+  return (req, res, next) => {
+    if (!req?.roles) return res.sendStatus(401);
+    const roles = [...allowedRoles];
+    console.log(req.roles, roles);
+    const result = req.roles
+      .map((role) => roles.includes(role))
+      .find((val) => val === true);
+    if (!result) return res.sendStatus(403);
+    next();
+  };
+};
+
+module.exports = verifyRoles;
