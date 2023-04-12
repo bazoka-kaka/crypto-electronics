8833763371feb1a30b0abfdcd107e066a9803291
bazoka-kaka <tsukikagemaikaka1@gmail.com>
2023-04-12 19:29:29 +0700

    add react router dom in react

--- crypto-fe/src/App.js
+++ crypto-fe/src/App.js
@@ -1,7 +1,20 @@
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import UnprotectedLayout from "./components/UnprotectedLayout";
 import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return <Home />;
+  return (
+    <Routes>
+      {/* unprotected */}
+      <Route path="/" element={<UnprotectedLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
