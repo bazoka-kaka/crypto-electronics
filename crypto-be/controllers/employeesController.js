79c827911928659c8b1a9083104a1c7045228577
bazoka-kaka <tsukikagemaikaka1@gmail.com>
2023-04-13 13:36:57 +0700

    remove employees dummy api

--- crypto-be/controllers/employeesController.js
+++
@@ -1,9 +0,0 @@
-const EmployeesDB = {
-  employees: require("../model/Employees.json"),
-};
-
-const getAllEmployees = (req, res) => {
-  res.json(EmployeesDB.employees);
-};
-
-module.exports = { getAllEmployees };
