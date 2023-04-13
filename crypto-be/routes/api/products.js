1b695360a40adcdeaed2996d94f36b317e1f12b7
bazoka-kaka <tsukikagemaikaka1@gmail.com>
2023-04-13 15:14:53 +0700

    add roles verification to products api

--- crypto-be/routes/api/products.js
+++ crypto-be/routes/api/products.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const router = express.Router();
 const productsController = require("../../controllers/productsController");
+const roles_list = require("../../config/roles_list");
+const verifyRoles = require("../../middlewares/verifyRoles");
 
 router
   .route("/")
   .get(productsController.getAllProducts)
-  .post(productsController.createNewProduct)
-  .put(productsController.updateProduct)
-  .delete(productsController.deleteProduct);
+  .post(
+    verifyRoles(roles_list.Admin, roles_list.Editor),
+    productsController.createNewProduct
+  )
+  .put(
+    verifyRoles(roles_list.Admin, roles_list.Editor),
+    productsController.updateProduct
+  )
+  .delete(
+    verifyRoles(roles_list.Admin, roles_list.Editor),
+    productsController.deleteProduct
+  );
 
 router.route("/:id").get(productsController.getProduct);
 
