7a691f99a724c069494bc0a842983f3ff03c19fe
bazoka-kaka <tsukikagemaikaka1@gmail.com>
2023-04-24 15:30:16 +0700

    update footer copyright

--- crypto-fe/src/components/Footer.jsx
+++ crypto-fe/src/components/Footer.jsx
@@ -3,19 +3,19 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer className="px-48 border-t-2 border-black pt-10 pb-24 flex justify-between items-start">
+    <footer className="flex items-start justify-between px-48 pt-10 pb-24 border-t-2 border-black">
       {/* title */}
       <div>
-        <h2 className="uppercase text-xl font-semibold tracking-wider">
+        <h2 className="text-xl font-semibold tracking-wider uppercase">
           Crypto Electronics
         </h2>
-        <p className="mt-4 font-light">&copy; bazoka-kaka, 2023</p>
+        <p className="mt-4 font-light">&copy; Volvinco Industries, 2023</p>
       </div>
       {/* Socials */}
 
       <div className="flex gap-24">
         <div>
-          <h2 className="font-semibold text-xl">Socials</h2>
+          <h2 className="text-xl font-semibold">Socials</h2>
           <ul className="flex flex-col gap-2 mt-4 font-light">
             <li>
               <a href="/">Github</a>
@@ -30,7 +30,7 @@ const Footer = () => {
         </div>
         {/* CTA */}
         <div>
-          <h2 className="font-semibold text-xl">Links</h2>
+          <h2 className="text-xl font-semibold">Links</h2>
           <ul className="flex flex-col gap-2 mt-4 font-light">
             <li>
               <Link to="/">Home</Link>
